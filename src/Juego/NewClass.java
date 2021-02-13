79477ce3ab23ced2628e1cbed045381f978dc88c
JosueDavid21 <71468537+JosueDavid21@users.noreply.github.com>
2021-02-13 16:33:48 -0500

    Creo clase

---
+++ src/Juego/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juego;
+
+/**
+ *
+ * @author Josue
+ */
+public class NewClass {
+
+    public NewClass() {
+    }
+    
+    public void nombre(String name){
+        System.out.println(name);
+    }
+}
