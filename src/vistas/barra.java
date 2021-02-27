cc9269c35ef4a62f8739903b9696d602d247bf1a
edgar <edgar@DESKTOP-B5JRV3M>
2021-02-27 18:22:57 -0500

    agregar parte de inicio

---
+++ src/vistas/barra.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vistas;
+
+import javax.print.attribute.standard.PresentationDirection;
+
+/**
+ *
+ * @author edgar
+ */
+public class barra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      VistaInicial pre = new VistaInicial();
+      pre.setVisible(true);
+      VistaMenu iniciar = new VistaMenu(pre, true);
+      try {
+          for (int i = 0; i <= 100; i++) {
+              Thread.sleep(80);
+              pre.progreso.setText(Integer.toString(i)+"%");
+              pre.barra.setValue(i);
+              
+              if(i==100){
+                  pre.setVisible(false);
+              iniciar.setVisible(true);
+          }
+          }
+      
+      }catch (Exception e){
+
+}
+    }
+    
+}
